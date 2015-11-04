7ba5a8dfc697f56178d3ddd741e668e09aee92d1
saasbook <saasbook@saasbook.(none)>
2015-11-04 05:18:33 -0400

    1.index view created

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+
+def creat
+	
+end
+
+def read
+	
+end
+
+def index
+	@posts = Post.all
+end
+
+def delete
+	
+end
+
+def edit
+	
+end
+
+def update
+
+end
+
+def destroy
+
+end
+
+
+
+
+
+end
